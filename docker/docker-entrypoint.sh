f6efbebae7f84caed2d632864d346266a8f35b91
Thomas M Steenholdt <tmus@tmus.dk>
2021-05-24 13:45:38 -0200

    Improve handling of mounted configs and add important defaults to configuration

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,10 +2,16 @@
 
 set -euo pipefail
 
-# Create config file based on PDNS_ environment variables
-while read -r setting; do
-    param="${setting/PDNS_/}"
-    echo "${param//_/-}=${!setting}"
-done < <(compgen -v "PDNS_") > /etc/powerdns/recursor.conf
+# Setup recommended defaults
+PDNS_lua_config_file=${PDNS_lua_config_file:="/etc/powerdns/recursor.lua"}
+
+# Create config file based on PDNS_ environment variable
+# - Skip if the file has been mounted from the outside
+if ! mountpoint -q "/etc/powerdns/recursor.conf"; then
+    while read -r setting; do
+        param="${setting/PDNS_/}"
+        echo "${param//_/-}=${!setting}"
+    done < <(compgen -v "PDNS_") > /etc/powerdns/recursor.conf
+fi
 
 exec "$@"
