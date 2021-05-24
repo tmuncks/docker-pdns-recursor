6546944127e1ab2cc0b67ae840b522b4b19a6d12
Thomas M Steenholdt <tmus@tmus.dk>
2021-05-24 14:51:33 -0200

    Revert to using built-in values for root hints and trust anchor

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -5,7 +5,6 @@ set -euo pipefail
 # Setup recommended defaults
 PDNS_local_address=${PDNS_local_address:="0.0.0.0"}
 PDNS_local_port=${PDNS_local_port:="53"}
-PDNS_lua_config_file=${PDNS_lua_config_file:="/etc/powerdns/recursor.lua"}
 
 # Create config file based on PDNS_ environment variable
 # - Skip if the file has been mounted from the outside
